8a91ed46f3abe17debe6231860ec03e7fc0f5fdf
emiliomurillo <emiliomurillo@localhost>
2022-02-18 10:06:17 -0700

    Version 1

---
+++ EVA1_1_HOLAMUNDO/src/eva1_1_holamundo/EVA1_1_HOLAMUNDO.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_1_holamundo;
+
+/**
+ *
+ * @author emiliomurillo
+ */
+
+/*
+    Palabras en azul: palabras reservadas: uso exclusivo del lenguaje
+    Palabras en negro: identificadores --> variables, constantes, clases, funciones objetos, etc.
+    []: arreglo
+    {}: delimitadores -> bloques de códigos
+
+*/
+
+public class EVA1_1_HOLAMUNDO { // CUERPO DE UNA CLASE
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) { // FUNCIÓN
+        // TODO code application logic here
+        System.out.println("Hello world!!");
+        //System.out.println(args[0]); //args en posición 0
+    }
+    
+}
+// .java -- Enlazador
+// .javac -- Compilador
+// .class - Compilado (Corre junto al .java)
+
+// Dos tipos de lenguajes: compilados --> toma un lenguaje entendido por los humanos, y lo traduce a lenguaje entendido por la computadora
+//interpretados - programa que funciona como traductor
